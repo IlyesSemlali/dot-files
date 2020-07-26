faffb36005bef98d3e5ee511ffbb6aaffefce3e0
Ilyès Semlali <ilyes.semlali@zenika.com>
2020-07-26 22:09:32 +0200

    feat: Configured base items
    * qtile
    * rofi
    * synclient

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,7 @@
+#! /bin/bash 
+
+nitrogen --restore &
+picom --experimental-backends -b &
+kdeconnect-app &
+klipper &
+unclutter --timeout 1 &
---
+++ .config/qtile/settings.py
@@ -0,0 +1,4 @@
+mod = "mod4"                                     # Sets mod key to SUPER/WINDOWS
+myTerm = "alacritty"                             # My terminal of choice
+myConfig = "/home/dt/.config/qtile/config.py"    # The Qtile config file location
+
