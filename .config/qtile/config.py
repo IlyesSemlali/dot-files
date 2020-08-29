69462713c05a453326e8d3732e3bcf0ae8294de3
Ilyès Semlali <ilyes.semlali@zenika.com>
2020-08-29 13:51:47 +0200

    feat: Configured Xmonad (switch to gentoo)
    - scratchpads
    - xmobar
    - custom shortcuts
    - fullscreen windows
    - confgured picom and alacritty

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -28,7 +28,7 @@ colors = [["#182828", "#182828"], # 0
           ["#FFFFFF", "#FFFFFF"]] # 7 window name
 
 group_names = [('a',"TERM", {'layout': 'monadtall'}),
-               ('q', "WWW", {'layout': 'max'}),
+               ('q', "WWW", {'layout': 'stack'}),
                ('w', "SYS", {'layout': 'monadtall'}),
                ('x', "RAND", {'layout': 'monadtall'})]
 
@@ -45,30 +45,30 @@ layout_theme = {"border_width": 0,
 layouts = [
     #layout.MonadWide(**layout_theme),
     #layout.Bsp(**layout_theme),
-    #layout.Stack(stacks=2, **layout_theme),
+    layout.Stack(num_stacks=1, **layout_theme),
     #layout.Columns(**layout_theme),
     #layout.RatioTile(**layout_theme),
     #layout.VerticalTile(**layout_theme),
     #layout.Matrix(**layout_theme),
     #layout.Zoomy(**layout_theme),
     layout.MonadTall(**layout_theme),
-    layout.Max(**layout_theme)
+    #layout.Max(**layout_theme),
     #layout.Tile(shift_windows=True, **layout_theme),
     #layout.Stack(num_stacks=2, **layout_theme),
-    #layout.TreeTab(
-    #     font = "Ubuntu",
-    #     fontsize = 10,
-    #     sections = ["FIRST", "SECOND"],
-    #     section_fontsize = 11,
-    #     bg_color = "141414",
-    #     active_bg = "90C435",
-    #     active_fg = "000000",
-    #     inactive_bg = "384323",
-    #     inactive_fg = "a0a0a0",
-    #     padding_y = 5,
-    #     section_top = 10,
-    #     panel_width = 320
-    #     ),
+    layout.TreeTab(
+         font = "Ubuntu",
+         fontsize = 10,
+         sections = ["Windows: "],
+         section_fontsize = 11,
+         bg_color = "141414",
+         active_bg = "90C435",
+         active_fg = "000000",
+         inactive_bg = "384323",
+         inactive_fg = "a0a0a0",
+         padding_y = 5,
+         section_top = 10,
+         panel_width = 1
+         )
     #layout.Floating(**layout_theme)
 ]
 
