0bcb453464b223f14144b079a54c08ed97a1dd88
Ilyès Semlali <ilyes.semlali@zenika.com>
2020-10-19 22:48:42 +0200

    chore: Split XMonad config, keybindings, NSP and worspaces

---
+++ .xmonad/lib/Config.hs
@@ -0,0 +1,19 @@
+module Config where
+
+import XMonad
+
+modMask :: KeyMask
+modMask = mod4Mask
+
+homeDir = "/home/ilyes/"
+wallpaper = ".config/wallpapers/pirogues.jpg"
+
+-- apps
+term = "alacritty"   -- Sets default terminal
+browser = "qutebrowser "               -- Sets firefox as browser for tree select
+editor = term ++ " -e vim "    -- Sets vim as editor for tree select
+
+-- commands 
+menuManager = "rofi -terminal alacritty -combi-modi window,drun,ssh -drun-show-actions -font 'hack 10' -show combi"
+wallpaperCommand = "feh --bg-fill " ++ wallpaper ++ "&"
+myXmobarCommand = "xmobar " ++ Config.homeDir ++ ".xmonad/xmobar.hs"
