3307e4c01d85ac50834ac0c5b876fbd2278dd8a4
Ilyès Semlali <ilyes.semlali@zenika.com>
2020-10-19 22:48:42 +0200

    chore: Split XMonad config, keybindings, NSP and worspaces

---
+++ .xmonad/lib/Scratchpads.hs
@@ -0,0 +1,69 @@
+module Scratchpads where
+
+import System.Exit (exitSuccess)
+import System.IO (hPutStrLn)
+
+import XMonad
+
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks, ToggleStruts(..))
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat, doRectFloat)
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.WorkspaceHistory
+
+import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
+import XMonad.Util.SpawnOnce
+import XMonad.Util.NamedScratchpad
+import qualified XMonad.StackSet as W
+
+import Config
+
+smallNSP = customFloating $ W.RationalRect l t w h
+               where
+                 h = 0.6
+                 w = 0.6
+                 t = 0.8 -h
+                 l = 0.8 -w
+
+mediumNSP = customFloating $ W.RationalRect l t w h
+               where
+                 h = 0.8
+                 w = 0.8
+                 t = 0.9 -h
+                 l = 0.9 -w
+
+fullNSP = customFloating $ W.RationalRect l t w h
+               where
+                 h = 1
+                 w = 1
+                 t = 1 -h
+                 l = 1 -w
+
+
+pads :: [NamedScratchpad]
+pads = [ NS "terminal" spawnTerm findTerm mediumNSP
+                , NS "keepass" spawnKeepass findKeepass smallNSP
+                , NS "weechat" spawnWeechat findWeechat mediumNSP
+                , NS "youtube-music" spawnMocp findMocp fullNSP
+                , NS "netflix" spawnNetflix findNetflix fullNSP
+                , NS "kdeconnect-sms" spawnKSMS findKSMS smallNSP
+                ]
+  where
+    spawnTerm  = Config.term ++ " -t scratchpad"
+    findTerm   = title=? "scratchpad"
+
+    spawnWeechat  = Config.term ++ " -t weechat -e weechat"
+    findWeechat   = title=? "weechat"
+
+    spawnKeepass  = "keepassxc"
+    findKeepass   = className=? "KeePassXC"
+
+    spawnMocp  = Config.browser ++ " --qt-arg name ytmusic --basedir .cache/qutebrowser-ytmusic music.youtube.com"
+    findMocp   = resource =? "ytmusic"
+
+    spawnKSMS = "kdeconnect-sms"
+    findKSMS = className =? "kdeconnect.sms"
+
+    spawnNetflix = "firefox --kiosk https://netflix.com"
+    findNetflix   = className =? "Firefox"
