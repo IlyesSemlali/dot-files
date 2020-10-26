03ea08ae8cce2a46fe3c494d6ce7e31516ffa6da
Ilyès Semlali <ilyes.semlali@zenika.com>
2020-10-26 22:21:37 +0100

    Fixed paths

--- .xmonad/lib/Config.hs
+++ .xmonad/lib/Config.hs
@@ -13,8 +13,8 @@ term = "alacritty"   -- Sets default terminal
 browser = "qutebrowser "               -- Sets firefox as browser for tree select
 editor = term ++ " -e vim "    -- Sets vim as editor for tree select
 
--- commands 
+-- commands
 menuManager = "rofi -terminal alacritty -combi-modi window,drun,ssh -drun-show-actions -font 'hack 10' -show combi"
 wallpaperCommand = "feh --bg-fill " ++ wallpaper ++ " &"
-myXmobarCommand = "xmobar " ++ Config.homeDir ++ ".xmonad/xmobar.hs"
+xmobarCommand = "xmobar -i " ++ homeDir ++ ".xmonad/xpm/ " ++ homeDir ++ ".xmonad/xmobar.hs"
 vmCommand = "vboxmanage startvm \"Windows 10 (base)\""
