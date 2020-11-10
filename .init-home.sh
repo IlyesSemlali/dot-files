8bfce5639a764077dbdf86264a57ccf0f348cc48
Ilyès Semlali <ilyes.semlali@zenika.com>
2020-11-10 23:08:17 +0100

    cleant init-home.sh and config

--- .init-home.sh
+++ .init-home.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-
-rm -rf .oh-my-zsh/
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git reset --hard
-
-sed "s/ilyes/$USER/g" -i ~/.xmonad/lib/Config.hs
-cd ~/.xmonad/lib/ && ghc --make Config.hs; cd
+XMONAD_CONFIG_TEMPLATE="/home/$USER/.xmonad/lib/Config.hs.tpl"
+XMONAD_CONFIG="/home/$USER/.xmonad/lib/Config.hs"
 
 
-sed "s/ilyes/$USER/g" -i ~/.config/nitrogen/bg-saved.cfg ~/.config/nitrogen/nitrogen.cfg 
+function install_vim_plugins() {
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	vim +PluginInstall +qall
+}
+
+function reset_omz() {
+	echo "-- Installing OhMyZSH --"
+
+	rm -rf .oh-my-zsh/
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	git -C ~ reset --hard
+}
+
+function _get_config_keys() {
+	grep '=' $1 | awk '{ print $1 }' | grep -v '^[\s-]*$'
+}
+
+function configure_xmonad() {
+	if [ ! -f $XMONAD_CONFIG ]
+	then
+		echo "-- Adding a fresh Xmonad config --"
+		sed "s/user/$USER/g" $XMONAD_CONFIG_TEMPLATE > $XMONAD_CONFIG
+	fi
+
+	for config_key in $(_get_config_keys $XMONAD_CONFIG_TEMPLATE)
+	do
+		if ! grep -q "^$config_key" $XMONAD_CONFIG
+		then
+			echo "-- Adding $config_key in Xmonad configuration --"
+			#grep "^$config_key" $XMONAD_CONFIG_TEMPLATE >> $XMONAD_CONFIG
+		fi
+	done
+
+	cd ~/.xmonad/lib/ && ghc --make Config.hs; cd
+}
+
+reset_omz
+configure_xmonad
--- .xmonad/lib/Config.hs.tpl
+++ .xmonad/lib/Config.hs.tpl
@@ -1,15 +1,20 @@
 module Config where
 
+import XMonad
+
+modMask :: KeyMask
+modMask = mod4Mask
+
 homeDir = "/home/user/"
+wallpaper = homeDir ++ ".config/wallpapers/pirogues.jpg"
 
-font = "xft:SourceCodePro:bold:size=9:antialias=true:hinting=true"
+-- apps
+term = "alacritty"   -- Sets default terminal
+browser = "qutebrowser "               -- Sets firefox as browser for tree select
+editor = term ++ " -e vim "    -- Sets vim as editor for tree select
 
+-- commands
 menuManager = "rofi -terminal alacritty -combi-modi window,drun,ssh -drun-show-actions -font 'hack 10' -show combi"
-
-terminalEmulator = "alacritty"
-
-browser = "qutebrowser "
-
-editor = terminalEmulator ++ " -e vim "
-
-wallpaper = ".wallpaper.jpg"
+wallpaperCommand = "feh --bg-fill " ++ wallpaper ++ " &"
+xmobarCommand = "xmobar -i " ++ homeDir ++ ".xmonad/xpm/ " ++ homeDir ++ ".xmonad/xmobar.hs"
+vmCommand = "vboxmanage startvm \"Awesome Linux VM (of course)\""
