48933d6f7cbcfca18cb0595dbe20634f8cfcdf04
Ilyès Semlali <ilyes.semlali@zenika.com>
2021-05-22 19:19:47 +0200

    now using manul locations

--- .config/redshift/hooks/backlight.sh
+++ .config/redshift/hooks/backlight.sh
@@ -6,10 +6,10 @@ echo "$(date +%F)" $@ >> /tmp/redshift_hook
 # Set brightness values for each status.
 # Range from 1 to 100 is valid
 
-brightness_day=85
-brightness_transition=40
+brightness_day=80
+brightness_transition=30
 brightness_evening=20
-brightness_night=15
+brightness_night=7
 
 set_brightness() {
 	kill $(pgrep -f '.*xbacklight.*')
@@ -21,9 +21,14 @@ if [ "$1" = period-changed ]; then
 		night)
 			current_epoch=$(date +%s)
 			target_epoch=$(date -d "22:00" +%s)
-			set_brightness $brightness_evening
-			sleep $(( $target_epoch - $current_epoch ))
-			set_brightness $brightness_night
+			if [ $(( $target_epoch - $current_epoch )) -lt 18000 ]
+			then
+				set_brightness $brightness_evening
+				sleep $(( $target_epoch - $current_epoch )) || true
+				set_brightness $brightness_night
+			else
+				set_brightness $brightness_night
+			fi
 
 			;;
 		transition)
