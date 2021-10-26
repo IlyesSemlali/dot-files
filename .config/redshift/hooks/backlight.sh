2b61e83596d251c66fbb446cf560d46c58ee915b
Ilyès Semlali <ilyes.semlali@zenika.com>
2021-10-26 10:58:22 +0200

    fix(backlight): fixed redshift hook

--- .config/redshift/hooks/backlight.sh
+++ .config/redshift/hooks/backlight.sh
@@ -1,25 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 # Set brightness via xbrightness when redshift status changes
 
 # Set brightness values for each status.
 # Range from 1 to 100 is valid
 
-brightness_day=80
-brightness_transition=30
+brightness_day=90
+brightness_transition=50
 brightness_evening=30
 brightness_night=20
 
 set_brightness() {
-	if ! [ -f /tmp/backlight.lock ]
+	if ! [ -e /tmp/backlight.lock ]
 	then
+		echo changing backlight
 		id -u > /tmp/backlight.lock
 		pkill -f '.*xbacklight.*'
-		xbacklight -time 30000 -fps 10 -set $1 &
+		xbacklight -time 20000 -fps 10 -set $1
 		rm /tmp/backlight.lock
 	fi
 }
 
-if [ "$1" = period-changed ]; then
+if [ "$1" == "period-changed" ]
+then
 	case $3 in
 		night)
 			current_epoch=$(date +%s)
