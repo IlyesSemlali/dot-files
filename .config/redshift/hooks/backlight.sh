ad9a2e88047880a18ba49490f61a5527de80d02d
Ilyès Semlali <ilyes.semlali@zenika.com>
2022-04-14 22:53:24 +0200

    feat(lock): add wrappers around volume / mic and backlight for better locking

--- .config/redshift/hooks/backlight.sh
+++ .config/redshift/hooks/backlight.sh
@@ -9,38 +9,31 @@ brightness_transition=50
 brightness_evening=30
 brightness_night=20
 
-set_brightness() {
-	if ! [ -e /tmp/backlight.lock ]
-	then
-		echo changing backlight
-		id -u > /tmp/backlight.lock
-		pkill -f '.*xbacklight.*'
-		xbacklight -time 20000 -fps 10 -set $1
-		rm /tmp/backlight.lock
-	fi
-}
-
 if [ "$1" == "period-changed" ]
 then
 	case $3 in
 		night)
+
 			current_epoch=$(date +%s)
 			target_epoch=$(date -d "22:00" +%s)
+
 			if [ $(( $target_epoch - $current_epoch )) -lt 18000 ]
 			then
-				set_brightness $brightness_evening
+				backlight soft_transition $brightness_evening
 				sleep $(( $target_epoch - $current_epoch )) || true
-				set_brightness $brightness_night
+				backlight soft_transition $brightness_night
 			else
-				set_brightness $brightness_night
+				backlight soft_transition $brightness_night
 			fi
 
 			;;
+
 		transition)
-			set_brightness $brightness_transition
+			backlight soft_transition $brightness_transition
 			;;
+
 		daytime)
-			set_brightness $brightness_day
+			backlight soft_transition $brightness_day
 			;;
 	esac
 fi
