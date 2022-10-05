1234cf8bb5b947d98a14af2682a0472f66e612ad
Ilyès Semlali <ilyes.semlali@zenika.com>
2022-10-05 09:49:42 +0200

    fix(init): install plugins in init

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -2,36 +2,4 @@ source ~/.local/share/homebrew/opt/zplug/init.zsh
 
 zplug "b4b4r07/enhancd", use:init.sh
 
-
-###########################
-# Nice Feauture of zplug: #
-###########################
-
-# # Load if "if" tag returns true
-# zplug "lib/clipboard", from:oh-my-zsh, if:"[[ $OSTYPE == *darwin* ]]"
-
-# # Run a command after a plugin is installed/updated
-# # Provided, it requires to set the variable like the following:
-# # ZPLUG_SUDO_PASSWORD="********"
-# zplug "jhawthorn/fzy", \
-#     as:command, \
-#     rename-to:fzy, \
-#     hook-build:"make && sudo make install"
-
-
-# # Can manage local plugins
-# zplug "~/.zsh", from:local
-
-#####################################################################
-# Install plugins if there are plugins that have not been installed #
-#####################################################################
-
-if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
-    fi
-fi
-
-# Then, source plugins and add commands to $PATH
 zplug load
