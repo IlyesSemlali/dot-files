c05f10e64111e3e15d64da8bad47cbbdd5cdc299
Ilyès Semlali <ilyes.semlali@zenika.com>
2022-10-19 21:31:40 +0200

    fix: disable luasnip

--- .config/nvim/after/plugin/completion.lua
+++ .config/nvim/after/plugin/completion.lua
@@ -35,8 +35,9 @@ cmp.setup {
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+      -- TODO: Fix LuaSnip
+      -- elseif luasnip.expand_or_jumpable() then
+      --   luasnip.expand_or_jump()
       else
         fallback()
       end
