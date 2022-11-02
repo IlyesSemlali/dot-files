eaa3f5ad83b4a35ae74f7137565c3692c610c1c2
Ilyès Semlali <ilyes.semlali@zenika.com>
2022-11-02 18:08:49 +0100

    fix(zsh): cache GNU paths to speed up zsh loading
    ZSH became soooooooo slow on first launch, the find command was slow
    so all we need to do is cache it somewhere and suffer from that only
    once

--- .zshrc.d/homebrew.zsh
+++ .zshrc.d/homebrew.zsh
@@ -6,3 +6,17 @@ then
 else
   log_error "homebrew" "homebrew not installed, please run '~/.init-home'"
 fi
+
+# Add paths to GNU tools using cache
+gnupaths="${HOME}/.cache/gnupaths"
+
+if [ ! -e ${gnupaths} ]; then
+    find ${HOME}/.local/share/homebrew/opt -type d -follow -name gnubin -print > ${gnupaths}
+fi
+
+for bindir in $(cat ${gnupaths}); do
+    insert_path "${bindir}"
+done
+
+unset gnupaths
+
