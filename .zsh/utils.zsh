d7b3bfd405a0bf0e88d364ba7912543fb3176856
Ilyès Semlali <ilyes.semlali@zenika.com>
2022-11-17 18:51:00 +0100

    fix(zsh): fix path

--- .zsh/utils.zsh
+++ .zsh/utils.zsh
@@ -3,6 +3,8 @@ function insert_path() {
     if ! echo $PATH | grep -q "$1:"
     then
         PATH="$1:$PATH"
+    else
+        PATH="$1:$(echo $PATH | sed "s@$1:@@g")"
     fi
 }
 
@@ -10,5 +12,7 @@ function append_path() {
     if ! echo $PATH | grep -q ":$1"
     then
         PATH="$PATH:$1"
+    else
+        PATH="$(echo $PATH | sed "s@:$1@@g"):$1"
     fi
 }
