41b1960d601c46df3099baaab4c2f9061aa0d3ff
Ilyès Semlali <ilyes.semlali@zenika.com>
2022-12-18 22:00:46 +0100

    feat(vim): Kickstart the utlimate lua nvim config
    Everything fits perfectly, this is great !

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,41 @@
+return(function(use)
+    -- Completion
+    -- use 'onsails/lspkind-nvim'
+    -- use 'hrsh7th/cmp-buffer'
+    -- use 'hrsh7th/cmp-cmdline'
+    -- use 'hrsh7th/cmp-vsnip'
+    -- use 'hrsh7th/vim-vsnip'
+
+
+
+    -- use 'Gavinok/SpaceWay.vim'
+    -- use 'airblade/vim-rooter'
+    use 'ap/vim-css-color'
+    -- use 'beloglazov/vim-online-thesaurus'
+    use 'christianrondeau/vim-base64'
+    use 'editorconfig/editorconfig-vim'
+    use 'gentoo/gentoo-syntax'
+    use 'hashivim/vim-terraform'
+    -- use 'lilydjwg/colorizer'
+    use 'machakann/vim-sandwich'
+    use 'martinda/Jenkinsfile-vim-syntax'
+    use 'mbbill/undotree'
+    use 'mhinz/vim-startify'
+    -- use 'rkitover/vimpager'
+    use 'rust-lang/rust.vim'
+    use 'ryanoasis/vim-devicons'
+    use 'samoshkin/vim-mergetool'
+    use 'towolf/vim-helm'
+    use 'tpope/vim-eunuch'
+    -- use 'vim-airline/vim-airline'
+    -- use 'vim-airline/vim-airline-themes'
+
+
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        }
+    }
+
+end)
