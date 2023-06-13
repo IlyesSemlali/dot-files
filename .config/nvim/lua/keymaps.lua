87836691ceabb23477bbe7985af8d338ed3b2b77
Ilyès Semlali <ilyes.semlali@zenika.com>
2023-06-13 15:37:13 +0200

    feat(nvim): add nvim tree shortcuts
    also remove deprecated config, and make it sticky

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -10,11 +10,16 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>DE', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>de', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 -- List maniuplation keymaps
 vim.keymap.set('n', '<leader>lt', "kmoj:.m0<CR>'o")
 vim.keymap.set('n', '<leader>lb', "jmok:.m$<CR>'o")
+
+-- Nvim-Tree bookmarks
+vim.keymap.set("n", "<leader>mn", require("nvim-tree.api").marks.navigate.next)
+vim.keymap.set("n", "<leader>mp", require("nvim-tree.api").marks.navigate.prev)
+vim.keymap.set("n", "<leader>ms", require("nvim-tree.api").marks.navigate.select)
