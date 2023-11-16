d98c57fde34688c10c1a571d326461cd841badf5
Ilyès Semlali <ilyes@macos>
2023-11-16 17:09:13 +0100

    feat(nvim): configure NvDash (won't start for now)

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -9,8 +9,54 @@ M.mappings = require "custom.mappings"
 
 -- UI
 M.ui = {
+
+  hl_override = {
+    NvDashAscii = {
+      fg = "blue",
+      bg = "black",
+    },
+
+    NvDashButtons = {
+      fg = "blue",
+      bg = "one_bg",
+    },
+  },
+
   theme = "gruvbox";
-  transparency = true
+  transparency = true,
+
+  nvdash = {
+    load_on_startup = true,
+
+    header = {
+      "          ________ ++     ________          ",
+      "         /VVVVVVVV\\++++  /VVVVVVVV\\         ",
+      "         \\VVVVVVVV/++++++\\VVVVVVVV/         ",
+      "          |VVVVVV|++++++++/VVVVV/'          ",
+      "          |VVVVVV|++++++/VVVVV/'            ",
+      "         +|VVVVVV|++++/VVVVV/'+             ",
+      "       +++|VVVVVV|++/VVVVV/'+++++           ",
+      "     +++++|VVVVVV|/VVVVV/'+++++++++         ",
+      "       +++|VVVVVVVVVVV/'+++++++++           ",
+      "         +|VVVVVVVVV/'+++++++++             ",
+      "          |VVVVVVV/'+++++++++               ",
+      "          |VVVVV/'+++++++++                 ",
+      "          |VVV/'+++++++++                   ",
+      "          'V/'   ++++++                     ",
+      "                   ++                       ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+
+
 }
 
 return M
