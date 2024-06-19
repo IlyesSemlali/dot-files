cb8ab7b86d2e6d620e68eceae357660117b9a1d2
Ilyès Semlali <ilyes@macos>
2024-06-19 14:34:46 +0200

    fix(init-home): remove tfswitch / krew
    those are installed through homebrew

--- .init-home.sh
+++ .init-home.sh
@@ -55,22 +55,6 @@ function install_tools() {
 
     # tmux powerline
     install_from_git https://github.com/erikw/tmux-powerline ~/.local/share/tmux-powerline
-
-    # tfswitch
-    if ! which tfswitch > /dev/null 2>&1; then
-        curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | sudo bash
-    fi
-
-    # Krew
-    (
-        cd "$(mktemp -d)" &&
-        OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-        ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-        KREW="krew-${OS}_${ARCH}" &&
-        curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-        tar zxvf "${KREW}.tar.gz" &&
-        ./"${KREW}" install krew
-    )
 }
 
 function configure_zsh() {
