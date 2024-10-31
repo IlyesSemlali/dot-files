34f7b969c477a775db6e0828d4cd97297099271c
Ilyès Semlali <ilyes@macos>
2024-10-31 15:18:06 +0100

    perf(zsh): use gnubin instead of using find + cache

--- .zsh/homebrew.zsh
+++ .zsh/homebrew.zsh
@@ -2,20 +2,7 @@
 if [ -f ${HOME}/.local/share/homebrew/bin/brew ]
 then
   eval $(${HOME}/.local/share/homebrew/bin/brew shellenv)
+  insert_path "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
 else
-  echo "init-home: brew not installed, please run 'init-home -b'"
+  echo "init-home: brew not installed, please run '~/.init-home'"
 fi
-
-# Add paths to GNU tools using cache
-gnupaths="${HOME}/.cache/gnupaths"
-
-if [ ! -e ${gnupaths} ]; then
-    find ${HOME}/.local/share/homebrew/opt -type d -follow -name gnubin -print > ${gnupaths}
-fi
-
-for bindir in $(cat ${gnupaths}); do
-    insert_path "${bindir}"
-done
-
-unset gnupaths
-
