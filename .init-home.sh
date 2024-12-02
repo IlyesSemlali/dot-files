7a9c612aeb9bfd36c600788f7775e22e51ff3f0f
Ilyès Semlali <ilyes@macos>
2024-12-02 16:57:03 +0100

    fix(init): remove tmux configuration

--- .init-home.sh
+++ .init-home.sh
@@ -2,27 +2,7 @@
 
 source "${HOME}/.local/lib/log"
 
-# HomeBrew
-if [ -f $HOME/.local/share/homebrew/bin/brew ]; then
-    eval $($HOME/.local/share/homebrew/bin/brew shellenv)
-fi
-
-
 PATH="$PATH:~/.local/bin/"
-
-function install_from_git() {
-    git_bin=$(which git)
-    repo=${1}
-    install_path=${2}
-
-    if [ -d ${install_path} ]; then
-        git -C ${install_path} pull
-    else
-        git clone ${repo} ${install_path}
-    fi
-    unset install_path
-    unset repo
-}
 
 function install_apt() {
     # Install from APT
@@ -30,15 +10,4 @@ function install_apt() {
     sudo apt install nvim bat zsh exa zoxide fzf node
 }
 
-function install_tpm() {
-
-    source ~/.zshrc
-
-    # tmux TPM
-    install_from_git https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    ${HOME}/.tmux/plugins/tpm/bin/clean_plugins
-    ${HOME}/.tmux/plugins/tpm/bin/install_plugins
-}
-
 install_apt
-install_tpm
