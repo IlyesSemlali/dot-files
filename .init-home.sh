37ef82c8873f5c69f29b417647c736308e9b0251
Ilyès Semlali <ilyes@macos>
2024-12-02 21:17:12 +0100

    fix(init): fix eza, nodejs, neovim and zoxide install

--- .init-home.sh
+++ .init-home.sh
@@ -34,9 +34,19 @@ function install_brew() {
 }
 
 function install_apt() {
-    # Install from APT
     sudo apt update
-    sudo apt install nvim bat zsh exa zoxide fzf node
+    sudo apt install -y bat fzf zsh gpg neovim nodejs
+
+    # Install zoxide
+    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+    # Install eza
+    sudo mkdir -p /etc/apt/keyrings
+    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+    sudo apt update
+    sudo apt install -y eza
 }
 
 function install_tpm() {
