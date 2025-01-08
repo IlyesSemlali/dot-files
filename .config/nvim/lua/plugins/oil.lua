7dd0a22f3702da3ee4a7f858ca2ccfff132b4c65
Ilyès Semlali <ilyes@macos>
2025-01-08 22:21:25 +0100

    feat(nvim): configure oil
    - open files in current window
    - set mappings for vsplit, tabs and more
    
    TODO: set a list of custom hidden files https://github.com/stevearc/oil.nvim/blob/7041528bdedb350ad66e650684deec8456e053cc/doc/oil.txt#L101C1-L104C13

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -10,41 +10,27 @@ return {
 		config = function()
 			local map = vim.keymap.set
 
-			map("n", "<leader><space>", ":Oil --float<CR>")
-
 			require("oil").setup({
 				keymaps = {
 					-- ["g?"] = { "actions.show_help", mode = "n" },
-					-- ["<CR>"] = "actions.select",
-					-- ["<C-s>"] = { "actions.select", opts = { vertical = true } },
-					-- ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
-					-- ["<C-t>"] = { "actions.select", opts = { tab = true } },
-					-- ["<C-p>"] = "actions.preview",
-					-- ["<C-c>"] = { "actions.close", mode = "n" },
-					-- ["<C-l>"] = "actions.refresh",
+					["<CR>"] = { "actions.select", opts = { close = true } },
+					["<C-v>"] = { "actions.select", opts = { vertical = true } },
+					["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+					["<C-t>"] = { "actions.select", opts = { tab = true } },
+					["<C-p>"] = "actions.preview",
+					["<Leader><Space>"] = { "actions.close", mode = "n" },
 					["-"] = { "actions.parent", mode = "n" },
-					-- ["_"] = { "actions.open_cwd", mode = "n" },
-					-- ["`"] = { "actions.cd", mode = "n" },
-					-- ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-					-- ["gs"] = { "actions.change_sort", mode = "n" },
-					-- ["gx"] = "actions.open_external",
-					-- ["g."] = { "actions.toggle_hidden", mode = "n" },
-					-- ["g\\"] = { "actions.toggle_trash", mode = "n" },
+					["_"] = { "actions.open_cwd", mode = "n" },
+					["<C-.>"] = { "actions.toggle_hidden", mode = "n" },
 				},
 				float = {
 					-- Padding around the floating window
-					padding = 10,
+					padding = 0,
 					-- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-					max_width = 50,
-					max_height = 30,
+					max_width = 100,
+					max_height = 40,
 					border = "rounded",
-					win_options = {
-						winblend = 0,
-					},
-					-- optionally override the oil buffers window title with custom function: fun(winid: integer): string
-					get_win_title = nil,
-					-- preview_split: Split direction: "auto", "left", "right", "above", "below".
-					preview_split = "auto",
+					preview_split = "right",
 					-- This is the config that will be passed to nvim_open_win.
 					-- Change values here to customize the layout
 					override = function(conf)
@@ -52,6 +38,10 @@ return {
 					end,
 				},
 			})
+
+			map("n", "<leader><space>", function()
+				require("oil").open_float()
+			end, { silent = true })
 		end,
 	},
 }
