93b7bb9257005c1073015396ddecf8b574dfca94
Ilyès Semlali <ilyes@macos>
2025-01-08 22:02:55 +0100

    fix(nvim): set transparent properly
    Options aren't handled properly by this pluging...
    I hate relying on people that do a poor job

--- .config/nvim/lua/plugins/transparent.lua
+++ .config/nvim/lua/plugins/transparent.lua
@@ -2,16 +2,16 @@ return {
 	{
 		"xiyaowong/transparent.nvim",
 		lazy = false,
-		opts = {
-			extra_groups = {
-				"NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
-				"DiagnosticSignError",
-				"DiagnosticSignWarn",
-				"DiagnosticSignInfo",
-				"DiagnosticSignHint",
-			},
-		},
 		config = function()
+			require("transparent").setup({
+				extra_groups = {
+					"NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+					"DiagnosticSignError",
+					"DiagnosticSignWarn",
+					"DiagnosticSignInfo",
+					"DiagnosticSignHint",
+				},
+			})
 			vim.cmd("TransparentEnable")
 		end,
 	},
