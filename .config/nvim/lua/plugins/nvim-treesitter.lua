d47067cf48e662b56012c9ac88c970327435a4d7
Ilyès Semlali <ilyes@macos>
2025-02-19 13:51:27 +0100

    fix(nvim): fix treesitter config, add support for function objects

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,27 +1,48 @@
 return {
 	{
+
 		"nvim-treesitter/nvim-treesitter",
-
-		opts = {
-			ensure_installed = {
-				"vim",
-				"lua",
-				"html",
-				"css",
-				"javascript",
-				"typescript",
-				"tsx",
-				"c",
-				"markdown",
-				"markdown_inline",
-			},
-
-			indent = {
-				enable = true,
-				-- disable = {
-				--   "python"
-				-- },
-			},
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
+		build = ":TSUpdate",
+
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"css",
+					"html",
+					"javascript",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"tsx",
+					"typescript",
+					"vim",
+				},
+
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+				},
+
+				indent = {
+					enable = true,
+					-- disable = {
+					--   "python"
+					-- },
+				},
+			})
+		end,
 	},
 }
