a6facc357f1f90a7299967b5c38c46fa93b484cb
Ilyès Semlali <ilyes.semlali@zenika.com>
2025-05-06 11:41:27 +0200

    feat(nvim): use nonlinewise '*' surrounds

--- .config/nvim/lua/plugins/vim-sandwich.lua
+++ .config/nvim/lua/plugins/vim-sandwich.lua
@@ -1,6 +1,20 @@
 return {
-  {
-    "machakann/vim-sandwich",
-    lazy = false,
-  },
+	{
+		"machakann/vim-sandwich",
+		lazy = false,
+		config = function()
+			local global = vim.g
+			local recipes = global["sandwich#default_recipes"]
+
+			table.insert(recipes, {
+				buns = { "*", "*" },
+				kind = { "add", "replace" },
+				action = { "add", "replace" },
+				input = { "*" },
+				linewise = 0,
+			})
+
+			global["sandwich#recipes"] = recipes
+		end,
+	},
 }
