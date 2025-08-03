becd8c720561a314451cbabc5427961d1769ab7d
Ilyès Semlali <ilyes@macos>
2025-08-03 20:01:32 +0200

    fix(nvim): remove mason-lspconfig
    lsp-config will be configured only through lsp-config, otherwise LSPs
    are attached twice on buffers

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,45 +3,10 @@
 -- and configured in conform (~/.config/nvim/lua/plugins/conform.lua)
 
 return {
-  {
-    "williamboman/mason.nvim",
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup {
-
-        -- List of available lspconfigs are present here:
-        -- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
-        ensure_installed = {
-          -- lua
-          "lua_ls",
-
-          -- Nix
-          "nil_ls",
-
-          -- Text
-          "ltex",
-
-          -- bash
-          "bashls",
-
-          -- python
-          "pyright",
-          "pylsp",
-
-          -- Ansible
-          "ansiblels",
-
-          -- Kubernetes
-          "yamlls",
-          "helm_ls",
-
-          -- Terraform
-          "terraformls",
-        },
-      }
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = function()
+			require("mason").setup()
+		end,
+	},
 }
