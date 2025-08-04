02d471da919a94767ab5cde7c31bff07e8527a56
Ilyès Semlali <ilyes@macos>
2025-08-04 10:34:03 +0200

    fix(nvim): properly set py-ls with ruff and black
    - remove duplicate LSPs for python
    - get default capabilities using nvim-cmp

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,24 +1,27 @@
 -- The null-ls is a complement to Mason and allows to install automatically
 -- formatters (which Mason cannot do on its own)
 return {
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
-    config = function()
-      local null_ls = require "mason-null-ls"
-      null_ls.setup {
-        automatic_installation = true,
-        ensure_installed = {
-          "black",
-          "stylua",
-          "jq",
-          "terraform_fmt",
-        },
-      }
-    end,
-  },
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			local mason_null_ls = require("mason-null-ls")
+
+			mason_null_ls.setup({
+				automatic_installation = true,
+				ensure_installed = {
+					"black",
+					"jq",
+					"python-lsp-server",
+					"ruff",
+					"stylua",
+					"terraform_fmt",
+				},
+			})
+		end,
+	},
 }
