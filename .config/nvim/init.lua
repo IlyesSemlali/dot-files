589ea1aebc51ec9c7526254b99b603f5af672c52
Ilyès Semlali <ilyes.sem@gmail.com>
2025-09-23 20:43:23 +0200

    feat(nvim): add support for custom nvim config
    in order to override some things without committing them in git

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,3 +7,9 @@ require("config.autocmds")
 require("config.lazy")
 require("config.diagnostics")
 require("config.lsp-config")
+
+-- Custom uncommited config
+local custom_config = vim.fn.stdpath("config") .. "/lua/config/custom.lua"
+if vim.fn.filereadable(custom_config) == 1 then
+	require("config.custom")
+end
