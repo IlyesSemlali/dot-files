70ad8a72fe9a0c3dace718ba5a25c836e0d97d1e
Ilyès Semlali <ilyes.sem@gmail.com>
2025-09-30 17:58:41 +0200

    feat(nvim): use ruff for python formatting

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    lazy = false,
-    config = function()
-      require("conform").setup {
-        log_level = vim.log.levels.DEBUG,
-        format_on_save = {
-          lsp_format = "fallback",
-          timeout_ms = 1000,
-        },
-        formatters_by_ft = {
-          json = { "jq" },
-          lua = { "stylua" },
-          python = { "black" },
-          terraform = { "terraform_fmt" },
-        },
-      }
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*",
-        callback = function(args)
-          require("conform").format { bufnr = args.buf }
-        end,
-      })
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		lazy = false,
+		config = function()
+			require("conform").setup({
+				log_level = vim.log.levels.DEBUG,
+				format_on_save = {
+					lsp_format = "fallback",
+					timeout_ms = 1000,
+				},
+				formatters_by_ft = {
+					json = { "jq" },
+					lua = { "stylua" },
+					python = { "ruff" },
+					terraform = { "terraform_fmt" },
+				},
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			})
+		end,
+	},
 }
