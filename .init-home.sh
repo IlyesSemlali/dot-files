1f5e28e52038a559abc0415a8994e225e30de5b4
Ilyès Semlali <ilyes.semlali@zenika.com>
2025-12-07 16:30:00 +0100

    fix(init): format file

--- .init-home.sh
+++ .init-home.sh
@@ -4,47 +4,46 @@ source "${HOME}/.local/lib/log"
 
 # HomeBrew
 if [[ "$(uname)" == "Darwin" ]]; then
-    eval $($HOME/.local/share/homebrew/bin/brew shellenv)
+  eval $($HOME/.local/share/homebrew/bin/brew shellenv)
 fi
-
 
 PATH="$PATH:~/.local/bin/"
 
 function install_from_git() {
-    git_bin=$(which git)
-    repo=${1}
-    install_path=${2}
+  git_bin=$(which git)
+  repo=${1}
+  install_path=${2}
 
-    if [ -d ${install_path} ]; then
-        git -C ${install_path} pull
-    else
-        git clone ${repo} ${install_path}
-    fi
-    unset install_path
-    unset repo
+  if [ -d ${install_path} ]; then
+    git -C ${install_path} pull
+  else
+    git clone ${repo} ${install_path}
+  fi
+  unset install_path
+  unset repo
 }
 
 function install_brew() {
-    # Brew for mac
-    install_from_git https://github.com/Homebrew/brew ~/.local/share/homebrew
+  # Brew for mac
+  install_from_git https://github.com/Homebrew/brew ~/.local/share/homebrew
 
-    source ~/.zshrc
+  source ~/.zshrc
 
-    brew install rcmdnk/file/brew-file
-    brew file install
+  brew install rcmdnk/file/brew-file
+  brew file install
 }
 
 function install_tpm() {
 
-    gnupaths="${HOME}/.cache/gnupaths"
-    rm ${gnupaths}
+  gnupaths="${HOME}/.cache/gnupaths"
+  rm ${gnupaths}
 
-    source ~/.zshrc
+  source ~/.zshrc
 
-    # tmux TPM
-    install_from_git https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    ${HOME}/.tmux/plugins/tpm/bin/clean_plugins
-    ${HOME}/.tmux/plugins/tpm/bin/install_plugins
+  # tmux TPM
+  install_from_git https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  ${HOME}/.tmux/plugins/tpm/bin/clean_plugins
+  ${HOME}/.tmux/plugins/tpm/bin/install_plugins
 }
 
 install_tpm
