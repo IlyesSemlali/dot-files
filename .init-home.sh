415f24093e0c54696146eb00b21eeb283da83d7d
Ilyès Semlali <ilyes.semlali@zenika.com>
2025-12-07 16:33:11 +0100

    chore(init): format init file

--- .init-home.sh
+++ .init-home.sh
@@ -34,20 +34,20 @@ function install_brew() {
 }
 
 function install_apt() {
-    sudo apt update
-    sudo apt install -y bat fzf zsh gpg nodejs build-essential
-    sudo snap install --beta nvim --classic
+  sudo apt update
+  sudo apt install -y bat fzf zsh gpg nodejs build-essential
+  sudo snap install --beta nvim --classic
 
-    # Install zoxide
-    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+  # Install zoxide
+  curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
-    # Install eza
-    sudo mkdir -p /etc/apt/keyrings
-    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-    sudo apt update
-    sudo apt install -y eza
+  # Install eza
+  sudo mkdir -p /etc/apt/keyrings
+  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+  sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+  sudo apt update
+  sudo apt install -y eza
 }
 
 function install_tpm() {
