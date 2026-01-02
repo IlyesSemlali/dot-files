a65622d10513aeb52b3d6d5e4c7c8050e963e581
Ilyès Semlali <ilyes.sem@gmail.com>
2026-01-02 17:54:30 +0100

    feat(nvim): close debugger windows and test summary when quitting vim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -15,3 +15,33 @@ autocmd({ "BufEnter" }, {
 		end
 	end,
 })
+
+-- Close Neotest and DAP windows when they are the last ones remaining
+vim.api.nvim_create_autocmd({ "QuitPre" }, {
+	callback = function()
+		local invalid_ft = {
+			"neotest-summary",
+			"neotest-output-panel",
+			"dapui_scopes",
+			"dapui_breakpoints",
+			"dapui_stacks",
+			"dapui_watches",
+			"dapui_console",
+			"dap-repl",
+		}
+
+		local wins = vim.api.nvim_list_wins()
+		-- If there is more than one window, we check if they are all "sidebar" types
+		if #wins > 1 then
+			for _, win in ipairs(wins) do
+				local buf = vim.api.nvim_win_get_buf(win)
+				local ft = vim.bo[buf].filetype
+
+				-- If the window we are looking at is in our list, close it
+				if vim.tbl_contains(invalid_ft, ft) then
+					vim.api.nvim_win_close(win, true)
+				end
+			end
+		end
+	end,
+})
