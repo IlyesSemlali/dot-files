3823e14c98f8038deb80f32e64ddc794d73011b8
Ilyès Semlali <ilyes.sem@gmail.com>
2026-02-03 09:26:34 +0100

    feat(nvim): add toggle keymap for RenderMarkdown

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,15 +1,23 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	opts = {
-		anti_conceal = {
-			enabled = false,
-		},
-		heading = {
-			width = "block",
-			left_pad = 2,
-			right_pad = 2,
-			min_width = 50,
-		},
-	},
+	config = function()
+		require("render-markdown").setup({
+			anti_conceal = {
+				enabled = false,
+			},
+			heading = {
+				width = "block",
+				left_pad = 2,
+				right_pad = 2,
+				min_width = 50,
+			},
+		})
+
+		local map = vim.keymap.set
+
+		map("n", "<leader>rr", function()
+			require("render-markdown").toggle()
+		end, { silent = true, desc = "RenderMarkdown: Toggle" })
+	end,
 }
