a58965e06f34438122613f6048a153c5bc0042a6
Ilyès Semlali <ilyes@mac.home>
2026-06-01 15:19:43 +0200

    fix(hombrew): load Home Manager managed homebrew installation

--- .zshrc.d/homebrew.zsh
+++ .zshrc.d/homebrew.zsh
@@ -1,20 +1,9 @@
-# HomeBrew
-if [ -f ${HOME}/.local/share/homebrew/bin/brew ]; then
-	eval $(${HOME}/.local/share/homebrew/bin/brew shellenv)
-	insert_path "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
-else
-	log_error "homebrew" "homebrew not installed, please run '~/.init-home'"
+# Load Homebrew environment (Apple Silicon Mac)
+if [ -f "/opt/homebrew/bin/brew" ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# Add paths to GNU tools using cache
-gnupaths="${HOME}/.cache/gnupaths"
-
-if [ ! -e ${gnupaths} ]; then
-	find ${HOME}/.local/share/homebrew/opt -type d -follow -name gnubin -print >${gnupaths}
+# Load Home Manager session variables (like LIBRARY_PATH for Nix-Darwin)
+if [ -e "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+  source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 fi
-
-for bindir in $(cat ${gnupaths}); do
-	insert_path "${bindir}"
-done
-
-unset gnupaths
