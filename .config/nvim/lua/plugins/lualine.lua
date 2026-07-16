c81474da0645b27f73dab510b0d27b04179ee3b8
Ilyès Semlali <ilyes@ise-macbook.local>
2026-07-16 17:40:11 +0200

    feat(nvim): configure lua_line
    file related stuff on the left, git related stuff on the right and
    location, position on the "center"

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,9 +7,17 @@ return {
 			require("lualine").setup({
 				options = {
 					globalstatus = true,
-					sections = {
-						lualine_x = { require("yaml_nvim").get_yaml_key_and_value },
-					},
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "filetype", "filename" },
+					lualine_c = { "location" },
+					-- TODO: use a function to disable YAML key when not in a YAML file
+					-- lualine_c = { "location", require("yaml_nvim").get_yaml_key_and_value },
+
+					lualine_x = { "progress" },
+					lualine_y = { "diff" },
+					lualine_z = { "branch" },
 				},
 			})
 		end,
