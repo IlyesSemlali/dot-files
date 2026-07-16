549e980205aaba5a12ab83d8001cab6913937c63
Ilyès Semlali <ilyes@ise-macbook.local>
2026-07-16 11:37:21 +0200

    chore(tmux): format tmux-powerline config

--- .config/tmux-powerline/segments/project.sh
+++ .config/tmux-powerline/segments/project.sh
@@ -4,19 +4,18 @@
 TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN="${TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN}"
 
 generate_segmentrc() {
-	read -d '' rccontents  << EORC
+  read -d '' rccontents <<EORC
 # This is a sed filter, it will be applied to the '\$PROJECT' variable to remove unneeded characters.
 export TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN="${TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN_DEFAULT}"
 EORC
-	echo "$rccontents"
+  echo "$rccontents"
 }
 
 run_segment() {
-        if [ -z $TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN ];
-        then
-          tmux display-message -p '#S' | sed 's/-[0-9]*$//'
-        else
-          tmux display-message -p '#S' | sed 's/-[0-9]*$//' | sed "s/$TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN//"
-        fi
-        return 0
+  if [ -z $TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN ]; then
+    tmux display-message -p '#S' | sed 's/-[0-9]*$//'
+  else
+    tmux display-message -p '#S' | sed 's/-[0-9]*$//' | sed "s/$TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN//"
+  fi
+  return 0
 }
