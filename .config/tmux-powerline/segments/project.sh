d9d65d245ef2091670ad7b31fd6e58448908aec9
Ilyès Semlali <ilyes@ise-macbook.local>
2026-07-16 13:52:33 +0200

    perf(tmux): improve segment print time
    it used to cause lags when drawing the screen on nvim
    also the strip pattern was a pain to write, now it supports
    native sed patterns instead of having to escaps sequences

--- .config/tmux-powerline/segments/project.sh
+++ .config/tmux-powerline/segments/project.sh
@@ -1,21 +1,16 @@
 # shellcheck shell=bash
 # Shows project name
 
-TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN="${TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN}"
-
-generate_segmentrc() {
-  read -d '' rccontents <<EORC
-# This is a sed filter, it will be applied to the '\$PROJECT' variable to remove unneeded characters.
-export TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN="${TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN_DEFAULT}"
-EORC
-  echo "$rccontents"
-}
-
 run_segment() {
-  if [ -z $TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN ]; then
-    tmux display-message -p '#S' | sed 's/-[0-9]*$//'
+  eval "$(tmux show-environment -s TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN)"
+
+  if [ -z "$TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN" ]; then
+    echo '#{s|-[0-9]*$||:session_name}'
+
   else
-    tmux display-message -p '#S' | sed 's/-[0-9]*$//' | sed "s/$TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN//"
+    echo "#{s|-[0-9]*$||;s|${TMUX_POWERLINE_SEG_PROJECT_STRIP_PATTERN}||:session_name}"
+
   fi
+
   return 0
 }
