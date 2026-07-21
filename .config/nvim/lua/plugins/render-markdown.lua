dda06b2ea5c90fc56c50736ac1414c820821e215
Ilyès Semlali <ilyes@mac.home>
2026-07-21 17:29:22 +0200

    fix(nvim): disable latex rendering
    showed up in checkhealth

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -3,15 +3,16 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("render-markdown").setup({
-			anti_conceal = {
-				enabled = false,
-			},
+			anti_conceal = { enabled = false },
+
 			heading = {
 				width = "block",
 				left_pad = 2,
 				right_pad = 2,
 				min_width = 50,
 			},
+
+			latex = { enabled = false },
 		})
 
 		local map = vim.keymap.set
